734b0ac0898e6c975a40d8ba0c0883cefaffb781
MingjieGao <19mg8@queensu.ca>
2023-11-24 15:35:21 -0500

    Database first try to build

--- Excel_Generator.py
+++ Excel_Generator.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import random
 
-# 这是您的名字列表
 names = [
     "James Smith",
     "Olivia Johnson",
@@ -35,16 +34,13 @@ names = [
     "Natalie Young"
 ]
 
-# 创建ID和签到次数列表
 ids = list(range(1, 31))
 sign_in_counts = [random.randint(1, 30) for _ in range(30)]
 
-# 创建一个数据框架
 df = pd.DataFrame({
     'ID': ids,
     'Name': names,
     'Sign-in Count': sign_in_counts
 })
 
-# 将数据框架写入Excel文件
 df.to_excel('names_and_sign_ins.xlsx', index=False)
