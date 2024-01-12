ddad3e9b32ee6506d58184754c1d84ae51422f0c
MingjieGao <19mg8@queensu.ca>
2024-01-12 14:46:18 -0500

    Database first try to build

--- Excel_Generator.py
+++ Excel_Generator.py
@@ -37,10 +37,14 @@ names = [
 ids = list(range(1, 31))
 sign_in_counts = [random.randint(1, 30) for _ in range(30)]
 
+first_names = [name.split()[0] for name in names]
+last_names = [name.split()[1] for name in names]
+
 df = pd.DataFrame({
     'ID': ids,
-    'Name': names,
-    'Sign-in Count': sign_in_counts
+    'First Name': first_names,
+    'Last Name': last_names,
+    'Sign-in-Count': sign_in_counts
 })
 
 df.to_excel('names_and_sign_ins.xlsx', index=False)
--- Full_Process.py
+++ Full_Process.py
@@ -5,13 +5,14 @@ mydb = create_connection()
 mycursor = mydb.cursor()
 
 student_id = input("ID: ")
+first_name = input("First Name: ")
 
-mycursor.execute("SELECT * FROM names_and_sign_ins WHERE ID = %s", (student_id,))
+mycursor.execute("SELECT * FROM names_and_sign_ins WHERE ID = %s AND `First Name` = %s", (student_id, first_name))
 result = mycursor.fetchone()
 
 if result is not None:
     mycursor.execute("UPDATE names_and_sign_ins SET `Sign-in-Count` = `Sign-in-Count` + 1 WHERE ID = %s", (student_id,))
-    print("Successfully sign in")
+    print("Successfully signed in")
     mydb.commit()
 else:
-    print("Failed sign in, no student information")
+    print("Failed sign in, no student information or incorrect first name")
