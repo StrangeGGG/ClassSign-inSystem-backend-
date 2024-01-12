6ba55b180897bf444e9b0b82ed45ea4b32e561be
MingjieGao <19mg8@queensu.ca>
2024-01-12 13:02:27 -0500

    Database first try to build

---
+++ Simulation_Sign_in.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="elec498g7",
+    database="database_v2"
+)
+
+mycursor = mydb.cursor()
+
+student_id = input("ID: ")
+
+mycursor.execute("UPDATE students SET sign_in_count = sign_in_count + 1 WHERE id = %s", (student_id,))
+
+mydb.commit()
+
+mydb.close()
